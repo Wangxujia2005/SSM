1490d3f2807d6bf0c2ad7f8010555e04436797d3
好朋友 <MiaXujia@outlook.com>
2025-11-05 16:37:07 +0800

    文件上传功能

--- springmvc-06-file/src/main/java/com/wxj/controller/FileController.java
+++ springmvc-06-file/src/main/java/com/wxj/controller/FileController.java
@@ -1,4 +1,4 @@
-package com.github.subei.controller;
+package com.wxj.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
